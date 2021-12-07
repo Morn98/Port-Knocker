33e6a169679aa4653f0b3d1e5e93b81f4d7626e1
Morn98 <nentwig@mail.hs-ulm.de>
2021-12-07 10:36:14 +0100

    Update

--- knocker.py
+++ knocker.py
@@ -17,6 +17,7 @@ parser.add_argument('targetPorts', type=int, nargs='+', help='target ports to kn
 parser.add_argument('-d', '--delay', type=int, default=500, help='delay between each port knock in milliseconds')
 args = parser.parse_args()
 
+# print entered arguments
 print("[+] Targethost: " + args.targetHost)
 print("[+] TargetPorts: " + ' '.join(map(str, args.targetPorts)))
 print("[+] Delay: " + str(args.delay) + " milliseconds")
